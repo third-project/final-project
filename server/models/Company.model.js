a4193157bfe7265b5e4d2083d4562963a49dc5bb
JoseLValdes <jolvaldgo@gmail.com>
2021-12-06 18:43:00 +0100

    New Model COmpany. ClockIn change name to ClockInOut. User employees change name for subodinate and add type array

---
+++ server/models/Company.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const companySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    foundationDate: { type: Date },
+    fiscalCode: { type: String },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Email not valid"],
+    },
+    phone: { type: Number, match: [/^[679]{1}[0-9]{8}$/, "Number not valid"] },
+    employees: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    logo: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Company = model("Company", companySchema);
+
+module.exports = Company;
