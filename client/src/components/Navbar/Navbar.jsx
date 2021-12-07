f4ab28d570fabf649df73c5dc8bcfe916038db0c
Tatiana Prada <tatiana.1227@hotmail.com>
2021-12-07 23:03:11 +0000

    pages folder created and signup styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = (props) => {
   return (
     <nav>
       <Link to={PATHS.HOMEPAGE} className="nav__projectName">
-        {CONSTS.CAPITALIZED_APP} - created with IronLauncher
+        {CONSTS.CAPITALIZED_APP} 
       </Link>
 
       <div className="nav__authLinks">
--- client/src/pages/LogIn/LogIn.jsx
+++ client/src/pages/LogIn/LogIn.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { login } from "../services/auth";
+import { login } from "../../services/auth";
 import { useNavigate } from "react-router-dom";
-import "./Signup";
-import * as PATHS from "../utils/paths";
-import * as USER_HELPERS from "../utils/userToken";
+import "../Signup/Signup";
+import * as PATHS from "../../utils/paths";
+import * as USER_HELPERS from "../../utils/userToken";
 
 export default function LogIn({ authenticate }) {
   const [form, setForm] = useState({
