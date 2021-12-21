e4582f51a952ade2ed8a4ad54c0d840ccb1b3a69
Raquel Hidalgo <raqhidcor@gmail.com>
2021-12-21 10:21:22 +0100

    task changes

--- server/models/Task.model.js
+++ server/models/Task.model.js
@@ -1,17 +1,17 @@
-const {Schema,model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const taskSchema = new Schema(
-{
-    description:{
-        type:String,
-        required: true, 
+  {
+    description: {
+      type: String,
+      required: true,
     },
     user: { type: Schema.Types.ObjectId, ref: "User" },
-} ,
-{
+  },
+  {
     timestamps: true,
   }
-)
+);
 
 const Task = model("Task", taskSchema);
 
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,43 +1,45 @@
-const router = require("express").Router()
-const mongoose = require("mongoose")
-const User = require("../models/User.model")
-const Task = require("../models/Task.model")
-const Session = require("../models/Session.model")
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const User = require("../models/User.model");
+const Task = require("../models/Task.model");
+const Session = require("../models/Session.model");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
-
-const isLoggedIn = require("../middleware/isLoggedIn")
-
-router.post("/create", async(req,res)=>{
+router.post("/create", async (req,res)=>{
     const {description} = req.body
     try{
-        const session = await Session.findById(accessToken).populate("user")
-        const user = session.user
-
-        if(req.body.description){
+        if (!description){
+            return res.status(400).json({errorMessage: "Please provide a task description"})
+        }
+        Task.findOne({description}).then((found)=>{
+            if(found){
+                return res.status(400).json({errorMessage:"This task has been already created"})
+            }
+        })
+        if(description){
             const newTask = await Task.create({
-                description: req.body.description
-            });
-            return res.status(200).json(newTask)
-        }else{
-            return res. status(400).json({errorMessage: "Please provide a task description"});
+                description: description,
+            })
+            const user = await User.findByIdAndUpdate(userId, {$push:{tasks:newTask._id}})
+            return res.status(200).json(newTask);
+        } else{
+            return res.status(400).json({errorMessage: "Please fill the task description"})
         }
     } catch(err){
+        res.status(500).json({errorMessage: error.message})
+    }
+})
+
+router.get("/my-tasks",async(req,res)=>{
+    const accessToken = req.headers.authorization;
+    try{
+        const session = await Session.findById(accessToken).populate("user");
+        const tasksArray = await Task.find({user:{$eq:session.user._id}}).populate("user")
+        res.status(200).json(tasksArray);
+    }catch(err){
         console.log(err)
         return res.status(500).json({errorMessage: err.toString()})
     }
 })
-
-router.get("/all-mine", isLoggedIn, async (req, res) => {
-  
-    const accessToken = req.headers.authorization;
-    try{
-      const session = await Session.findById(accessToken).populate("user");
-      const taskArray = await Task.find({user:{$eq: session.user._id}}).populate("user");
-      return res.status(200).json(taskArray);
-    }catch(err){
-      console.log(err)
-      return res.status(500).json({errorMessage: err.toString()});
-    }
-  });
 
 module.exports = router;
