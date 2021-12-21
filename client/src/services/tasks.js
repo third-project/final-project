8e2db7681c6bfbc9eaf0268501095c70f43744b1
Raquel Hidalgo <raqhidcor@gmail.com>
2021-12-21 10:20:15 +0100

    task functionality changes

--- client/src/services/tasks.js
+++ client/src/services/tasks.js
@@ -2,30 +2,30 @@ import axios from "axios";
 import * as USER_HELPERS from "../utils/userToken";
 import { internalServerError, successStatus } from "./auth";
 
-const tasksService = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/tasks`,
-  headers: { Authorization: USER_HELPERS.getUserToken() },
+const taskService = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/task`
 });
 
-
-export function createTask(task) {
-  return tasksService
-  .post("/create",task)
+export function createTask(task){
+  return taskService
+  .post("/create", task,{
+    headers:{
+      Authorization: USER_HELPERS.getUserToken()
+    }
+  })
   .then(successStatus)
   .catch(internalServerError)
 }
 
-
-export async function getMyTasks() {
-  try {
-    const responseAllMyTasks = await tasksService.get("/all-mine", {
+export async function getMyTasks(){
+  try{
+    const responseMyTasks = await taskService.get("/my-tasks",{
       headers: {
         Authorization: USER_HELPERS.getUserToken()
       }
     });
-    return successStatus(responseAllMyTasks);
-  } catch (err) {
+    return successStatus(responseMyTasks);
+  }catch(err){
     return internalServerError(err);
   }
 }
-
